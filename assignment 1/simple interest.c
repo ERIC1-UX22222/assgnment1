20f07c8803aa1ae5fe0c7b6c5a68855cf1cf263c
ERIC1-UX22222 <ricospoiler2004@gmail.com>
2024-10-03 17:55:15 +0300

    Add files via upload

---
+++ assignment 1/simple interest.c
@@ -0,0 +1,18 @@
+/*
+Auther:Eric Mwaura Ndungu.
+Reg No:CT102/G/18621/23.
+Date:29/9/2024
+*/
+//Declaration and initialisation
+#include <stdio.h>
+//Program to calculate simple interest
+ int main (){
+    int Principalamount=20000;
+    int time=12;
+    int rate=4;
+    int simpleinterest=(Principalamount*time*rate)/100;
+    printf("The simpleinterest is\t%d",simpleinterest);
+    return 0;
+
+ }
+ 
